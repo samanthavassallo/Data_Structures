05b37c2d79528801a2de473b6e3de45fac8d75eb
samanthavassallo <117092466+samanthavassallo@users.noreply.github.com>
2024-05-16 20:54:20 -0400

    Add files via upload

---
+++ IndexNode.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndexNode {
+
+	// The word for this entry
+	String word;
+	// The number of occurrences for this word
+	int occurences;
+	// A list of line numbers for this word.
+	List<Integer> list;
+
+	IndexNode left;
+	IndexNode right;
+
+
+	// Constructors
+	// Constructor should take in a word and a line number
+	// it should initialize the list and set occurrences to 1
+
+	public IndexNode(String word, int lineNum) {
+		this.word = word;
+		list = new ArrayList<>();
+		list.add(lineNum);
+		occurences=1;
+
+	}
+
+
+	// Complete This
+	// return the word, the number of occurrences, and the lines it appears on.
+	// string must be one line
+
+	public String toString() {
+		return word + " " + occurences + " " + list;
+
+
+	}
+}
